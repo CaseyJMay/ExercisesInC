d461e822a1c719d28b36e11f45182b2e88660de9
CaseyJMay <42981072+CaseyJMay@users.noreply.github.com>
2020-05-01 11:16:14 -0400

    Adding ex09 files

--- exercises/ex09/newshound.c
+++ exercises/ex09/newshound.c
@@ -13,11 +13,19 @@ Modified by Allen Downey.
 #include <sys/types.h>
 #include <wait.h>
 
+extern int errno;
 
 void error(char *msg)
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
     exit(1);
+}
+
+double get_seconds() {
+    struct timeval tv[1];
+
+    gettimeofday(tv, NULL);
+    return tv->tv_sec + tv->tv_usec / 1e6;
 }
 
 int main(int argc, char *argv[])
@@ -38,15 +46,53 @@ int main(int argc, char *argv[])
     int num_feeds = 5;
     char *search_phrase = argv[1];
     char var[255];
+    int status;
+    pid_t pid;
+    double start, stop;
+
+    // get the start time
+    start = get_seconds();
 
     for (int i=0; i<num_feeds; i++) {
         sprintf(var, "RSS_FEED=%s", feeds[i]);
         char *vars[] = {var, NULL};
 
-        int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
-        if (res == -1) {
-            error("Can't run script.");
+        pid = fork();
+
+        /* check for an error */
+        if (pid == -1) {
+            fprintf(stderr, "fork failed: %s\n", strerror(errno));
+            perror(argv[0]);
+            exit(1);
+        }
+
+        /* see if we're the parent or the child */
+        if (pid == 0) {
+            int res = execle(PYTHON, PYTHON, SCRIPT, search_phrase, NULL, vars);
+            if (res == -1) {
+                error("Can't run script.");
+            }
+            exit(i);
         }
     }
-    return 0;
+
+for (int i=0; i<num_feeds; i++) {
+        pid = wait(&status);
+
+        if (pid == -1) {
+            fprintf(stderr, "wait failed: %s\n", strerror(errno));
+            perror(argv[0]);
+            exit(1);
+        }
+
+        // check the exit status of the child
+        status = WEXITSTATUS(status);
+        if(status != 0){
+            printf("Child %d exited with error code %d.\n", pid, status);
+        }
+    }
+    // compute the elapsed time
+    stop = get_seconds();
+    printf("Elapsed time = %f seconds.\n", stop - start);
+    exit(0);
 }
